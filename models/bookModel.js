0ad83b08f1d358295a793269a40e66f6e37e84f8
Mujib <mujib517@gmail.com>
2021-11-10 10:51:21 +0530

    custom validations

--- models/bookModel.js
+++ models/bookModel.js
@@ -3,7 +3,15 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
     name: { type: String, minLength: 3, maxlength: 20, required: true },
-    price: { type: Number, required: true },
+    price: {
+        type: Number, required: true,
+        validate: {
+            validator: function (val) {
+                return val > 0 && val < 1000;
+                // return /\d{10}/.test(val);
+            }
+        }
+    },
     createdAt: { type: Date, default: Date.now() },
     updatedAt: { type: Date, default: Date.now() }
 });
